51c33b0e8a61d744b6eadfa25bf5d05b4b80730d
sksqlrlek <sksqlrlek@gmail.com>
2026-05-21 14:12:15 +0900

    feat: 메신저 기능 Vo 사전 작업  - lowerCamelCase 로 수정

--- src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrChtrmVO.java
+++ src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrChtrmVO.java
@@ -9,17 +9,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MsngrChtrmVO {
-	private Long ChtrmId;		// 채팅방 ID	- PK
+	private Long chtrmId;		// 채팅방 ID	- PK
 	
-	private String ChtrmNm;		// 채팅방 이름
-	private String ChtrmExpln;	// 채팅방 설명
-	private String ChtrmTypeCd;	// 채팅방 타입 코드
-	private Long EstblshId;		// 개설자 ID(TB_MEMBER)
+	private String chtrmNm;		// 채팅방 이름
+	private String chtrmExpln;	// 채팅방 설명
+	private String chtrmTypeCd;	// 채팅방 타입 코드
+	private Long estblshId;		// 개설자 ID(TB_MEMBER)
 	
-	private LocalDateTime CreatDt;		// 개설일시
-	private LocalDateTime EndDt;			// 종료일시
+	private LocalDateTime creatDt;		// 개설일시
+	private LocalDateTime endDt;			// 종료일시
 	
-	private List<MsngrChtrmPtcptVO> MsngrChtrmPtcpt; // 참여자 목록
-	private List<MsngrMsgVO> MsngrMsg; // 메시지 목록
+	private List<MsngrChtrmPtcptVO> msngrChtrmPtcpt; // 참여자 목록
+	private List<MsngrMsgVO> msngrMsg; // 메시지 목록
 	
 }
--- src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrMsgVO.java
+++ src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrMsgVO.java
@@ -8,12 +8,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MsngrMsgVO {
-	private Long MsgId;				// 메시지 ID - PK
-	private Long ChtrmId;			// 채팅방 ID(MSNGR_CHTRM) - FK
+	private Long msgId;				// 메시지 ID - PK
+	private Long chtrmId;			// 채팅방 ID(MSNGR_CHTRM) - FK
 	
-	private Long SndrId;			// 발신자 ID(TB_MEMBER)
+	private Long sndrId;			// 발신자 ID(TB_MEMBER)
 	
-	private String MsgCn;			// 발신 내용
+	private String msgCn;			// 발신 내용
 	
-	private LocalDateTime CreatDt;	// 발신 일시
+	private LocalDateTime creatDt;	// 발신 일시
 }
