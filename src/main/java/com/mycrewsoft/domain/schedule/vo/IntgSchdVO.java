6881ac16345cb9bd38921ba701fa1298f18a20cc
sksqlrlek <sksqlrlek@gmail.com>
2026-05-21 12:06:02 +0900

    feat: 일정 관리 기능 Vo 사전 작업  - IntgSchdVO 타입 수정

--- src/main/java/com/mycrewsoft/domain/schedule/vo/IntgSchdVO.java
+++ src/main/java/com/mycrewsoft/domain/schedule/vo/IntgSchdVO.java
@@ -1,5 +1,7 @@
 package com.mycrewsoft.domain.schedule.vo;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,23 +11,23 @@ public class IntgSchdVO {
 
 	private Long SchdId;		// 일정 ID
 	
-	private Long SchdClsfCd;	// 일정 구분 코드
+	private String SchdClsfCd;	// 일정 구분 코드
 	
-	private Long DeptCd;		// 부서 코드
-	private Long ProjId;		// 프로젝트 코드
+	private String DeptCd;		// 부서 코드
+	private String ProjId;		// 프로젝트 코드
 	
-	private Long SchdNm;		// 일정명
-	private Long SchdDetailCn;	// 일정 상세 내용
-	private Long BeginDt;		// 시작 일시
-	private Long EndDt;			// 종료 일시
+	private String SchdNm;		// 일정명
+	private String SchdDetailCn;	// 일정 상세 내용
+	private LocalDateTime BeginDt;		// 시작 일시
+	private LocalDateTime EndDt;			// 종료 일시
 	
 	private Long SchdWrtrId;	// 일정 작성자 ID
-	private Long SchdRegstDt;	// 일정 등록 일시
+	private LocalDateTime SchdRegstDt;	// 일정 등록 일시
 	
 	private Long SchdChgrId;	// 일정 수정자 ID
-	private Long SchdChgDt;		// 일정 변경 일시
+	private LocalDateTime SchdChgDt;		// 일정 변경 일시
 	
-	private Long DelYn;			// 삭제 여부
-	private Long DelDt;			// 삭제 일시
+	private String DelYn;			// 삭제 여부
+	private LocalDateTime DelDt;			// 삭제 일시
 	
 }
