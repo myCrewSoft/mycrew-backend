43bde8baebc42ca316e9f7e7642d9058953e5af8
sksqlrlek <sksqlrlek@gmail.com>
2026-05-21 12:09:40 +0900

    feat: 메신저 기능 Vo 사전 작업  - MsngrChtrmVO, MsngrMsgVO 타입 수정

--- src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrChtrmVO.java
+++ src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrChtrmVO.java
@@ -1,5 +1,6 @@
 package com.mycrewsoft.domain.messenger.vo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.Getter;
@@ -10,12 +11,13 @@ import lombok.Setter;
 public class MsngrChtrmVO {
 	private Long ChtrmId;		// 채팅방 ID	- PK
 	
-	private Long ChtrmNm;		// 채팅방 이름
-	private Long ChtrmExpln;	// 채팅방 설명
-	private Long ChtrmTypeCd;	// 채팅방 타입 코드
+	private String ChtrmNm;		// 채팅방 이름
+	private String ChtrmExpln;	// 채팅방 설명
+	private String ChtrmTypeCd;	// 채팅방 타입 코드
 	private Long EstblshId;		// 개설자 ID(TB_MEMBER)
-	private Long CreatDt;		// 개설일시
-	private Long EndDt;			// 종료일시
+	
+	private LocalDateTime CreatDt;		// 개설일시
+	private LocalDateTime EndDt;			// 종료일시
 	
 	private List<MsngrChtrmPtcptVO> MsngrChtrmPtcpt; // 참여자 목록
 	private List<MsngrMsgVO> MsngrMsg; // 메시지 목록
--- src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrMsgVO.java
+++ src/main/java/com/mycrewsoft/domain/messenger/vo/MsngrMsgVO.java
@@ -1,15 +1,19 @@
 package com.mycrewsoft.domain.messenger.vo;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class MsngrMsgVO {
-	private Long MsgId;		// 메시지 ID - PK
+	private Long MsgId;				// 메시지 ID - PK
+	private Long ChtrmId;			// 채팅방 ID(MSNGR_CHTRM) - FK
 	
-	private Long ChtrmId;	// 채팅방 ID(MSNGR_CHTRM)
-	private String SndrId;	// 발신자 ID(TB_MEMBER)
-	private Long MsgCn;		// 발신 내용
-	private Long CreatDt;	// 발신 일시
+	private Long SndrId;			// 발신자 ID(TB_MEMBER)
+	
+	private String MsgCn;			// 발신 내용
+	
+	private LocalDateTime CreatDt;	// 발신 일시
 }
