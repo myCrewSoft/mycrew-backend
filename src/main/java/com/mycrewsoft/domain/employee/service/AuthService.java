ba9ced972c1d28f2b6ff50b9bf8cb52fd0479502
MrLLIm <dnjsgh0084@naver.com>
2026-05-28 09:08:36 +0900

    docs: 인증/인가 체계 요약 txt 파일 추가

--- src/main/java/com/mycrewsoft/domain/employee/service/AuthService.java
+++ src/main/java/com/mycrewsoft/domain/employee/service/AuthService.java
@@ -3,7 +3,14 @@ package com.mycrewsoft.domain.employee.service;
 import com.mycrewsoft.domain.employee.dto.request.LoginRequestDTO;
 import com.mycrewsoft.domain.employee.dto.response.LoginResponseDTO;
 
+/**
+ * 사용자의 인증 관련된 비즈니스 로직을 처리하는 서비스 인터페이스.
+ */
 public interface AuthService {
+	/**
+	 * @param request
+	 * @return 로그인 요청에 대한 응답으로 액세스 토큰, 리프레시 토큰, 사번, 권한 버전, 첫 로그인 여부를 포함하는 DTO 객체
+	 */
 	LoginResponseDTO login(LoginRequestDTO request);
 	
 }
