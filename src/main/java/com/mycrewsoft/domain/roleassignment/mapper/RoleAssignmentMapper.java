9afb7ce7e13043640dc01ee4df904494e82dfb9e
MrLLIm <dnjsgh0084@naver.com>
2026-05-29 10:06:00 +0900

    feat: SQL 쿼리문의 카테시안 곱 우려 해결.

--- src/main/java/com/mycrewsoft/domain/roleassignment/mapper/RoleAssignmentMapper.java
+++ src/main/java/com/mycrewsoft/domain/roleassignment/mapper/RoleAssignmentMapper.java
@@ -1,5 +1,7 @@
 package com.mycrewsoft.domain.roleassignment.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,6 +11,8 @@ import com.mycrewsoft.domain.roleassignment.vo.RoleAssignmentVO;
 public interface RoleAssignmentMapper {
 
 	Long selectRoleIdByRoleCode(@Param("roleCd") String roleCd);
+
+	List<RoleAssignmentVO> selectRoleAssignmentsByEmpId(@Param("empId") Long empId);
 
 	int insertRoleAssignmentIfAbsent(RoleAssignmentVO roleAssignment);
 }
