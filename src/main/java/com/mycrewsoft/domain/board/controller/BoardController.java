86f193592c147676ebe4808ddd08df90efd252d3
biju97 <sdjne0620@gmail.com>
2026-06-01 16:55:47 +0900

    feat:게시판 사이드바의 목록조회

--- src/main/java/com/mycrewsoft/domain/board/controller/BoardController.java
+++ src/main/java/com/mycrewsoft/domain/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.mycrewsoft.domain.board.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page; // 💡 Page 임포트 추가
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mycrewsoft.common.response.ApiResponse;
 import com.mycrewsoft.domain.board.dto.request.BoardSearchRequest;
 import com.mycrewsoft.domain.board.dto.response.BoardResponse;
+import com.mycrewsoft.domain.board.dto.response.BoardSideBarResponse;
 import com.mycrewsoft.domain.board.service.BoardService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -30,15 +33,29 @@ public class BoardController {
 	
 	private final BoardService service;
 
-	@Operation(summary = "게시글 목록 조회 (페이징 적용)")
-	@GetMapping
-	public ResponseEntity<ApiResponse<Page<BoardResponse>>> getRes(
-			@Validated @ModelAttribute BoardSearchRequest searchRequest 
+	/*
+	 * @Operation(summary = "게시글 목록 조회 (페이징 적용)")
+	 * 
+	 * @GetMapping public ResponseEntity<ApiResponse<Page<BoardResponse>>> getRes(
+	 * 
+	 * @Validated @ModelAttribute BoardSearchRequest searchRequest ){ // 1. 서비스의 페이징
+	 * 메서드(getBoard)를 호출하고 Page 객체로 받습니다. Page<BoardResponse> boardPage =
+	 * service.getBoard(searchRequest);
+	 * 
+	 * // 2. 응답 데이터 타입을 Page<BoardResponse>로 일치시켜 전송합니다. return
+	 * ResponseEntity.ok(ApiResponse.success("게시판 목록 불러오기 성공!", boardPage)); }
+	 */
+	
+	@Operation(summary = "게시판 사이드바 조회")
+	@GetMapping("/sidebar")
+	public ResponseEntity<ApiResponse<List<BoardSideBarResponse>>> getSideBar(
+			
 	){
-		// 1. 서비스의 페이징 메서드(getBoard)를 호출하고 Page 객체로 받습니다.
-		Page<BoardResponse> boardPage = service.getBoard(searchRequest);
+	
+		List<BoardSideBarResponse> sideBar = service.getSideBar(); 
 		
-		// 2. 응답 데이터 타입을 Page<BoardResponse>로 일치시켜 전송합니다.
-		return ResponseEntity.ok(ApiResponse.success("게시판 목록 불러오기 성공!", boardPage));
+		return ResponseEntity.ok(ApiResponse.success(sideBar));
 	}
+	
+	
 }
