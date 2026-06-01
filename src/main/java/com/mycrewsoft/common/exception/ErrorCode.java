b669fd664308b382d385ac163e4c5fd897d813d8
Tamoon <sksqlrlek@gmail.com>
2026-06-02 07:44:40 +0900

    feat: 알림 도메인 ErrorCode 추가 - NOTIF_001: 존재하지 않는 알림 (404 NOT_FOUND)

--- src/main/java/com/mycrewsoft/common/exception/ErrorCode.java
+++ src/main/java/com/mycrewsoft/common/exception/ErrorCode.java
@@ -37,6 +37,9 @@ public enum ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "USER_002", "이미 사용 중인 이메일입니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "USER_003", "이미 사용 중인 닉네임입니다."),
 
+    // NOTIFICATION
+    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIF_001", "존재하지 않는 알림입니다."),
+
     // FILE
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_001", "파일 업로드에 실패했습니다."),
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "FILE_002", "허용되지 않는 파일 형식입니다."),
