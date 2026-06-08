38825558f75ac1fc877a72a792b3b12348532662
yunhanoida <yunhano48@gmail.com>
2026-06-08 12:20:00 +0900

    fix : 프로젝트 컨트롤러 수정

--- src/main/java/com/mycrewsoft/domain/project/controller/ProjectController.java
+++ src/main/java/com/mycrewsoft/domain/project/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.mycrewsoft.domain.project.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import com.mycrewsoft.domain.project.dto.ProjectCreateRequestDto;
 import com.mycrewsoft.domain.project.service.ProjectService;
 import com.mycrewsoft.validate.groups.InsertGroup;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +25,8 @@ import lombok.RequiredArgsConstructor;
 public class ProjectController {
 	private final ProjectService projService;
 	
+	@Operation(summary = "프로젝트 등록")
+	@PostMapping
 	public ResponseEntity<ApiResponse<String>> creatProject(
 		@Validated(InsertGroup.class) @RequestBody ProjectCreateRequestDto reqDto
 	){
